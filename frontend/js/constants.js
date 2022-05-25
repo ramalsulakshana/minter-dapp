0de50aa0ad904b738ebd6281c68a1a76f99185ba
Ramal Sulakshana <ramal.gamage@gmail.com>
2022-05-25 14:14:03 +0530

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xBC61e9539F0619aAfFd1D755283c9304102E86bF"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
